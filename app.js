435ffafaa4d33980f3406502ff400e16dac5131a
Anjalant Nelson <nelson.anjalant4@gmail.com>
2022-12-08 21:30:02 -0700

    5th commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const game = document.getElementById('game')
 const scoreDisplay = document.getElementById('score')
+document.addEventListener('contextmenu', event => event.preventDefault());
+
 
 const jeopardyCategories = [
 	{
@@ -74,27 +76,124 @@ const jeopardyCategories = [
 	},
 	{
 		genre: 'Video Games',
-		questions: []
+		questions: [
+			{
+				question: "the song 'Anaconda' by Nicki Minaj samples what song?",
+				answer: ["Baby Got Back", "Can't touch this"],
+				correct: "Baby Got Back",
+				level: "easy"
+			},
+			{
+				question: "Kendrick lamar was swimming in a pool full of what?",
+				answer: ["money", "liqour"],
+				correct: "liqour",
+				level: "medium"
+			},
+			{
+				question: "what song by Nas was made as a response to Jay-Z during their feud, and became a verb in rap?",
+				answer: ["Control", "Ether"],
+				correct: "Ether",
+				level: "hard"
+			},
+			{
+				question: "What rap group has a album that cannot be commercially exploited until 2103?",
+				answer: ["Wu-Tang Clan", "N.W.A"],
+				correct: "Wu-Tang Clan",
+				level: "expert"
+			},
+			{
+				question: "The 1995 Source awards caused what controversial battle to erupt?",
+				answer: ["west vs. east", "biggie vs. tupac"],
+				correct: "west vs. east",
+				level: "pro"
+			}
+		]
 	},
 	{
 		genre: 'Fellow Classmates',
-		questions: []
+		questions: [
+			{
+				question: "the song 'Anaconda' by Nicki Minaj samples what song?",
+				answer: ["Baby Got Back", "Can't touch this"],
+				correct: "Baby Got Back",
+				level: "easy"
+			},
+			{
+				question: "Kendrick lamar was swimming in a pool full of what?",
+				answer: ["money", "liqour"],
+				correct: "liqour",
+				level: "medium"
+			},
+			{
+				question: "what song by Nas was made as a response to Jay-Z during their feud, and became a verb in rap?",
+				answer: ["Control", "Ether"],
+				correct: "Ether",
+				level: "hard"
+			},
+			{
+				question: "What rap group has a album that cannot be commercially exploited until 2103?",
+				answer: ["Wu-Tang Clan", "N.W.A"],
+				correct: "Wu-Tang Clan",
+				level: "expert"
+			},
+			{
+				question: "The 1995 Source awards caused what controversial battle to erupt?",
+				answer: ["west vs. east", "biggie vs. tupac"],
+				correct: "west vs. east",
+				level: "pro"
+			}
+		]
 	},
 	{
 		genre: 'Social Media',
-		questions: []
+		questions: [
+			{
+				question: "the song 'Anaconda' by Nicki Minaj samples what song?",
+				answer: ["Baby Got Back", "Can't touch this"],
+				correct: "Baby Got Back",
+				level: "easy"
+			},
+			{
+				question: "Kendrick lamar was swimming in a pool full of what?",
+				answer: ["money", "liqour"],
+				correct: "liqour",
+				level: "medium"
+			},
+			{
+				question: "what song by Nas was made as a response to Jay-Z during their feud, and became a verb in rap?",
+				answer: ["Control", "Ether"],
+				correct: "Ether",
+				level: "hard"
+			},
+			{
+				question: "What rap group has a album that cannot be commercially exploited until 2103?",
+				answer: ["Wu-Tang Clan", "N.W.A"],
+				correct: "Wu-Tang Clan",
+				level: "expert"
+			},
+			{
+				question: "The 1995 Source awards caused what controversial battle to erupt?",
+				answer: ["west vs. east", "biggie vs. tupac"],
+				correct: "west vs. east",
+				level: "pro"
+			}
+		]
 	}
 ]
 
 let score = 0
 
 function addCategory(category) {
+	// $(window).on('load', function () {
+	// 	$('#loading').hide();
+	//   })
+
 	const column = document.createElement('div')
 	column.classList.add('genre-column')
 
 	const genreTitle = document.createElement('div')
 	genreTitle.classList.add('genre-title')
-	genreTitle.innerText = category.genre
+	genreTitle.innerHTML = category.genre
 
 	column.appendChild(genreTitle)
 	game.append(column)
@@ -135,23 +234,23 @@ function addCategory(category) {
 jeopardyCategories.forEach(category => addCategory(category))
 
 function flipCard() {
-	this.innerHTML = ""
-	this.style.fontSize = '15px'
+	this.InnerHTML = ""
+	this.style.fontSize = "15px"
 	this.style.lineHeight = "30px"
-	const textDisplay = document.getElementById('div')
+	const textDisplay = document.createElement('div')
 	textDisplay.classList.add('card-text')
-	textDisplay.innerHTML = this.getAttribute('data-question')
+	textDisplay.InnerHTML = this.getAttribute('data-question')
 	const firstButton = document.createElement('button')
 	const secondButton = document.createElement('button')
 	firstButton.classList.add('first-button')
-	secondButton.classList.add('secondf-button')
-	firstButton.innerHTML = this.getAttribute('data-answer-1')
-	secondButton.innerHTML = this.getAttribute('data-answer-2')
+	secondButton.classList.add('second-button')
+	firstButton.InnerHTML = this.getAttribute('data-answer-1')
+	secondButton.InnerHTML = this.getAttribute('data-answer-2')
 	firstButton.addEventListener('click', getResult)
 	secondButton.addEventListener('click', getResult)
 	this.append(textDisplay, firstButton, secondButton)
 
-	const allCards = Array.from(document.querySelector('.card'))
+	const allCards = Array.from(document.querySelectorAll('.card'))
 	allCards.forEach(card => card.removeEventListener('click', flipCard))
 }
 
@@ -160,8 +259,8 @@ function getResult() {
 	allCards.forEach(card => card.addEventListener('click', flipCard))
 	const cardOfButton = this.parentElement
 
-	if (cardOfButton.getAttribute('data-correct') == this.innerHTML) {
-		score = score + parseInt (cardOfButton.getAttribute('data-value'))
+	if (cardOfButton.getAttribute('data-correct') == this.InnerHTML) {
+		score = score + parseInt(cardOfButton.getAttribute('data-value'))
 		scoreDisplay.innerHTML = score
 		cardOfButton.classList.add('correct-answer')
 		setTimeout(() => {
